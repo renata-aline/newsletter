068e052ae48d2d1847b8351c1f9b0b343631af0b
renata-aline <renatalinec@gmail.com>
2024-10-07 17:16:19 -0300

    Implementação do modo noturno do projeto com Tailwind;

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
+
 const Header = () => {
-    return <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-        <span className="text-gray-100">Olá,usuário</span>
-        <h1 className="text-gray-100 text-xl">newsletter</h1>
-        <input type="checkbox" className="hidden sm:block"/>
+    return <div className="flex h-20 bg-blue-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+        <span className="text-gray-100">Olá, Usuário</span>
+        <h1 className="text-gray-100 text-xl">Newsletter</h1>
+        < ToggleTheme />
     </div>
 }
 export default Header;
